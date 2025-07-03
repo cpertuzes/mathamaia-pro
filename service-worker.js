904616eec41c5a300887a5634486e6e17c013854
cpertuzes <cpertuzes@ucchristus.cl>
2025-07-03 18:33:19 -0400

    Subir MathAmaia Pro

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+// Service Worker para MathAmaia Pro
+// Permite funcionar offline y como app instalable
+
+const CACHE_NAME = 'mathamaia-v1';
+const urlsToCache = [
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activar Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
